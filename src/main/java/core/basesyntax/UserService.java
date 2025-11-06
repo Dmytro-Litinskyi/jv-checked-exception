8e74f948fe930c11ebaa5681afd68dfffa105940
Dmytro_Litinskyi <litinskiupro100dima@gmail.com>
2025-11-06 02:57:53 +0100

    Methods registerUser and validate were implemented

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,18 @@
 package core.basesyntax;
 
 public class UserService {
+    private final PasswordValidator passwordValidator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        String password = user.getPassword();
+        String repeatPassword = user.getRepeatPassword();
+
+        try {
+            passwordValidator.validate(password, repeatPassword);
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            System.out.println("Your passwords are incorrect. Try again.");
+        }
     }
 
     public void saveUser(User user) {
